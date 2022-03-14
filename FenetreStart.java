6cd049f4fa64426f26c618004659a297f2d7532b
loulou0359 <louismuller59@gmail.com>
2022-03-14 09:42:44 +0100

    lancement start

--- FenetreStart.java
+++ FenetreStart.java
@@ -35,7 +35,7 @@ public class FenetreStart extends JFrame implements ActionListener{
 		
 		start1= new JButton("START");
 		start1.setBounds(450,800,300,100);
-		start1.setBackground(new Color(249,200,93));
+		start1.setBackground(new Color(249,200,0));
 		start1.setFont(new Font("Agency FB",Font.BOLD,50));
 		start1.setForeground(new Color(52,62,162));
 		start1.addActionListener(this);
@@ -47,7 +47,7 @@ public class FenetreStart extends JFrame implements ActionListener{
 		ombre.setText("Poisson");
 		ombre.setFont(new Font("Agency FB",Font.BOLD,180));
 		ombre.setForeground(Color.black);
-		ombre.setBackground(new Color(52,62,162));
+		ombre.setBackground(new Color(200,62,162));
 		
 		JLabel txt = new JLabel();
 		txt.setLayout(null);
@@ -60,7 +60,7 @@ public class FenetreStart extends JFrame implements ActionListener{
 		
 		String T = readFile("texte/debut.txt");
         JTextArea Texte = new JTextArea(T);
-        Texte.setBackground(new Color(52,62,162));
+        Texte.setBackground(new Color(52,0,0));
         Texte.setForeground(Color.white);
         Texte.setBounds(50,200,1200,600);
         Texte.setFont(new Font("Agency FB",Font.BOLD,25));
@@ -70,14 +70,12 @@ public class FenetreStart extends JFrame implements ActionListener{
         JPanel panneauGlobal = new JPanel();
 		panneauGlobal.setLayout(null);
 		panneauGlobal.setBounds(0,0,1000,1000);
-		panneauGlobal.setBackground(new Color(52,62,162));
+		panneauGlobal.setBackground(new Color(52,62,50));
 		panneauGlobal.add(start1);
 		panneauGlobal.add(txt);
 		panneauGlobal.add(Texte);
 		panneauGlobal.add(ombre);
 		
-		
-		Son.addActionListener(this);
 		
 		this.add(panneauGlobal);
 		this.setVisible(true);
