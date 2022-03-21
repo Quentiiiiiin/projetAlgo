dbc241e0aa735f9a4742c2fddf5f248204e2814d
loulou0359 <louismuller59@gmail.com>
2022-03-21 17:12:38 +0100

    fentre finale fonctionelle

--- FenetreFinale.java
+++ FenetreFinale.java
@@ -9,7 +9,7 @@ import java.io.*; //image
 public class FenetreFinale extends JFrame implements MouseListener, ActionListener{
 	
 	//<!fenêtre principale de jeu
-	private FenetreJeu fenetre;
+	//private FenetreJeu fenetre;
 	
 	//<!zone où seront affichées les infos sur les poissons
 	private JLabel Info;
@@ -22,11 +22,11 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
    * 
    * @param FenetreJeu f : objet désignant la fenêtre principale de jeu
    */
-    public FenetreFinale (FenetreJeu f) throws IOException {
+    public FenetreFinale (/*FenetreJeu f*/) {
 				
 		//initialisation de la fenêtre
 		
-		fenetre=f;
+		//fenetre=f;
         this.setTitle("Att\u00e9rissage sur Mars");
 		this.setSize(1000,1000);
 		this.setLocation(400,20);
@@ -38,11 +38,11 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
         JPanel Fond = new JPanel();
         Fond.setLayout(null);
         Fond.setBounds(0,0,1000,1000);
-        Fond.setBackground(new Color(52,62,162));
+        Fond.setBackground(new Color(52,62,162)); //changer couleur 
         
         JLabel Gif = new JLabel();
         Gif.setBounds(0,0,1000,1000);
-        Gif.setBackground(new Color(52,62,162));
+        Gif.setBackground(new Color(52,62,162)); //changer couleur 
         Fond.add(Gif);
         
         JLabel I1 = new JLabel(new ImageIcon("images/photo_mars.png"));
@@ -55,8 +55,8 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
         
         Info = new JLabel("+ D'INFOS");
         Info.setBounds(700,520,150,50);
-        Info.setBackground(new Color(52,62,162));
-        Info.setForeground(new Color(249,200,93));
+        Info.setBackground(new Color(52,62,162)); //changer couleur 
+        Info.setForeground(new Color(249,200,93)); //changer couleur 
         Info.setFont(new Font("Agency FB",Font.BOLD,35));
         Info.addMouseListener(this);
         Gif.add(Info);
@@ -64,20 +64,20 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
         Restart = new JButton("Restart");
         Restart.setBounds(730,30,200,75);
         Restart.setFont(new Font("Agency FB",Font.BOLD,35));
-        Restart.setForeground(new Color(52,62,162));
-        Restart.setBackground(new Color(249,200,93));
+        Restart.setForeground(new Color(52,62,162)); //changer couleur 
+        Restart.setBackground(new Color(249,200,93)); //changer couleur 
         Gif.add(Restart);
         
         JPanel cadreJaune = new JPanel();
         cadreJaune.setLayout(null);
         cadreJaune.setBounds(40,40,620,520);
-        cadreJaune.setBackground(new Color(249,200,93));
+        cadreJaune.setBackground(new Color(249,200,93)); //changer couleur 
         Gif.add(cadreJaune);
         
-        String T = readFile("texte/description.txt");
+        String T = readFile("description.txt");
         JTextArea Texte = new JTextArea(T);
-        Texte.setBackground(new Color(249,200,93));
-        Texte.setForeground(new Color(52,62,162));
+        Texte.setBackground(new Color(249,200,93)); //changer couleur 
+        Texte.setForeground(new Color(52,62,162)); //changer couleur 
         Texte.setBounds(45,610,900,330);
         Texte.setFont(new Font("Agency FB",Font.BOLD,30));
         Texte.setEditable(false);
@@ -87,35 +87,21 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
         JPanel fondTexte2 = new JPanel();
         fondTexte2.setLayout(null);
         fondTexte2.setBounds(45,610,900,330);
-        fondTexte2.setBackground(new Color(249,200,93));
+        fondTexte2.setBackground(new Color(249,200,93)); //changer couleur 
         Gif.add(fondTexte2);
         
         
         JPanel fondTexte = new JPanel();
         fondTexte.setLayout(null);
         fondTexte.setBounds(35,600,920,350);
-        fondTexte.setBackground(new Color(249,200,93));
+        fondTexte.setBackground(new Color(249,200,93)); //changer couleur 
         Gif.add(fondTexte);
         
-        Son = new JButton();
-        if(f.audioOn){
-			Son.setIcon(new ImageIcon("images/son on.png"));
-		}else{
-			Son.setIcon(new ImageIcon("images/son off.png"));
-		}
-		Son.setSize(50,50);
-		Son.setLocation(900,520);
-		Son.setBackground(new Color(52,62,162));
-		Gif.add(Son);
         
         this.add(Fond);
         
-        this.setVisible(true);
-        if(f.audioOn){
-			son.enBoucle();
-		}        
+        this.setVisible(true);      
         Restart.addActionListener(this);
-        Son.addActionListener(this); 
         
     }
     
@@ -126,23 +112,11 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
    */ 
     public void actionPerformed(ActionEvent e){
 		if(e.getSource()==Restart){
-			fenetre.restart();
-			son.arret();
-			restart.lecture();
+			//fenetre.restart();
+			//restart.lecture();
 			setVisible(false);
-		}
-		if(e.getSource()==Son){
-			if(fenetre.audioOn){
-				son.arret();
-				fenetre.audioOn=false;
-				Son.setIcon(new ImageIcon("images/son off.png"));
-			}else{
-				son.enBoucle();
-				fenetre.audioOn=true;
-				Son.setIcon(new ImageIcon("images/son on.png"));
-			}
-		}
-	}
+        }
+    }
     
 	/**
    * \fn void mouseEntered(MouseEvent e) : méthode permettant d'afficher la fenêtre d'infos supplémentaires si l'utilisateur passe sa souris sur le bouton Info
@@ -151,7 +125,7 @@ public class FenetreFinale extends JFrame implements MouseListener, ActionListen
    */
 	public void mouseEntered(MouseEvent e){
 		if(e.getSource() == Info){
-			new FenetreInfo(); //création de la fenêtre info
+			//new FenetreInfo(); //création de la fenêtre info
 		}
 	}
 	
