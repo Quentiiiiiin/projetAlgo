2d7252cd15fef5253d1716aa5506a41bb543d347
loulou0359 <louismuller59@gmail.com>
2022-03-25 19:48:53 +0100

    début changement AffJeu

--- AffichageJeu.java
+++ AffichageJeu.java
@@ -12,30 +12,29 @@ public class AffichageJeu extends JPanel{
 	//<!fenêtre principale correspondante
 	private FenetreJeu f;
 	
-	//<!tableau contenant les objets célestes du niveau 1
-	private ObjetsCelestes[] o1 = new ObjetsCelestes[3];
+	//<!tableau contenant les objets du niveau 1
+	private ElementMarin[] o1 = new ElementMarin[3];
 	
-	//<!tableau contenant les objets célestes du niveau 2
-	private ObjetsCelestes[] o2 = new ObjetsCelestes[3];
+	//<!tableau contenant les objets du niveau 2
+	private ElementMarin[] o2 = new ElementMarin[3];
 	
-	//<!tableau contenant les objets célestes du niveau 3
-	private ObjetsCelestes[] o3 = new ObjetsCelestes[4];
+	//<!tableau contenant les objets du niveau 3
+	private ElementMarin[] o3 = new ElementMarin[4];
 	
-	//<!tableau contenant les objets célestes du niveau 4
-	private ObjetsCelestes[] o4 = new ObjetsCelestes[5];
+	//<!tableau contenant les objets du niveau 4
+	private ElementMarin[] o4 = new ElementMarin[5];
 	
-	//<!tableau contenant les objets célestes du niveau 5
-	private ObjetsCelestes[] o5 = new ObjetsCelestes[6];
+	//<!tableau contenant les objets du niveau 5
+	private ElementMarin[] o5 = new ElementMarin[6];
 	
 	//<!booléen permettant de savoir si un carburant a été touché dans le niveau en cours
-	boolean touchCarbu=false; //
+	boolean toucherNourriture=false;
 	
-	//<!booléen gérant l'affichage ou non du carburant
+	//<!booléen gérant l'affichage ou non de la nourriture 
 	boolean affiche=true;
 	
 	//<!image de fond de la zone de jeu
-	private BufferedImage fondEtoile;
-	
+	private BufferedImage fondMarin;
 	/**
    * \fn AffichageJeu(FenetreJeu f) : constructeur AffichageJeu
    * 
@@ -44,33 +43,33 @@ public class AffichageJeu extends JPanel{
 	public AffichageJeu(FenetreJeu f) throws IOException{
 		
 		this.f=f;
-		fondEtoile= ImageIO.read(new File("images/fond_etoile.jpg")); //l'image de fond ne peut pas être créée dans la méthode paint (trop de ralentissement créé par la boucle try catch
+		fondMarin= ImageIO.read(new File("images/fond_etoile.jpg")); //l'image de fond ne peut pas être créée dans la méthode paint (trop de ralentissement créé par la boucle try catch
 		
 		//initialisation des tableaux d'objets
 		
-		o1[0] = new Carburant(1,f);
-		o2[0] = new Carburant(2,f);
-		o3[0] = new Carburant(3,f);
-		o4[0] = new Carburant(1,f);
-		o5[0] = new Carburant(1,f);
+		o1[0] = new Nourriture(1,f);
+		o2[0] = new Nourriture(2,f);
+		o3[0] = new Nourriture(3,f);
+		o4[0] = new Nourriture(1,f);
+		o5[0] = new Nourriture(1,f);
 		
-		o1[1] = new Meteorites(f);
-		o2[1] = new Meteorites(f);
-		o3[1] = new Meteorites(f);
-		o4[1] = new Meteorites(f);
-		o5[1] = new Meteorites(f);
-        o3[3] = new Meteorites(f);
-        o4[3] = new Meteorites(f);
-        o4[4] = new Meteorites(f);
-        o5[3] = new Meteorites(f);
-        o5[4] = new Meteorites(f);
+		o1[1] = new Requin(f);
+		o2[1] = new Requin(f);
+		o3[1] = new Requins(f);
+		o4[1] = new Requin(f);
+		o5[1] = new Requin(f);
+		o1[2] = new Requin(f);
+		o2[2] = new Requin(f);
+		o3[2] = new Requin(f);
+		o4[2] = new Requin(f);
+		o5[2] = new Requin(f);
+        o5[5] = new Requin(f);
+        o3[3] = new Requin(f);
+        o4[3] = new Requin(f);
+        o4[4] = new Requin(f);
+        o5[3] = new Requin(f);
+        o5[4] = new Requin(f);
 		
-		o1[2] = new TrouNoir(f);
-		o2[2] = new TrouNoir(f);
-		o3[2] = new TrouNoir(f);
-		o4[2] = new TrouNoir(f);
-		o5[2] = new TrouNoir(f);
-        o5[5] = new TrouNoir(f);
 	}
 	
 	/**
@@ -81,12 +80,12 @@ public class AffichageJeu extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
-		//dessin de l'image du rover, de la plateforme et du fond
+		//dessin de l'image du poisson, de la plateforme et du fond
 		
-		g.drawImage(fondEtoile,0,0,(int)(20.0/27.0*1500),(int)(5.0/6.0*1000),null);
-		g.setColor(new Color(249,200,93));
+		g.drawImage(fondMarin,0,0,(int)(20.0/27.0*1500),(int)(5.0/6.0*1000),null);
+		g.setColor(new Color(249,200,93)); //changer couleur 
 		g.fillRect((int)(3.5/5.0*f.largeurJeu),(int)(4.5/5.0*f.hauteurJeu),(int)(1.5/5.0*f.largeurJeu),(int)(0.2/5.0*f.hauteurJeu));
-		g.drawImage(f.Perseverance.image,(int)(f.Perseverance.origine.x),(int)(f.Perseverance.origine.y),f.Perseverance.largeur,f.Perseverance.hauteur,null);
+		g.drawImage(f.Nemo.image,(int)(f.Nemo.origine.x),(int)(f.Nemo.origine.y),f.Nemo.largeur,f.Nemo.hauteur,null);
 		
 		if(!f.enJeu){ //si l'utilisateur n'est pas en jeu
 		
@@ -94,8 +93,8 @@ public class AffichageJeu extends JPanel{
 		
 			g.setColor(new Color(249,200,93));
 			for(int i=0;i<10;i++){
-				g.drawOval((int)(f.vI*Math.cos(Math.toRadians(f.aI))*(i+1)+f.Perseverance.largeur),
-				(int)(f.gravite*Math.pow(i+1,2)-f.vI*Math.sin(Math.toRadians(f.aI))*(i+1)+833-f.Perseverance.hauteur),
+				g.drawOval((int)(f.vI*Math.cos(Math.toRadians(f.aI))*(i+1)+f.Nemo.largeur),
+				(int)(f.gravite*Math.pow(i+1,2)-f.vI*Math.sin(Math.toRadians(f.aI))*(i+1)+833-f.Nemo.hauteur),
 				5,5);
 			}
 		}
@@ -116,7 +115,7 @@ public class AffichageJeu extends JPanel{
 					g.drawImage(o1[i].image,(int)(o1[i].origine.x),(int)(o1[i].origine.y),100,100,null);
 				}
 			}
-			toucheObjetCeleste(o1);
+			toucheElementMarin(o1);
 		}else if(f.numNiveau==2){
 			o2[0].origine = new APoint(400.0,300.0);
 			o2[1].origine = new APoint(300.0,550.0);
@@ -130,7 +129,7 @@ public class AffichageJeu extends JPanel{
 					g.drawImage(o2[i].image,(int)(o2[i].origine.x),(int)(o2[i].origine.y),100,100,null);
 					}
 			}
-			toucheObjetCeleste(o2);
+			toucheElementMarin(o2);
 		}else if(f.numNiveau==3){
 			o3[0].origine = new APoint(500.0,300.0); //carburant
 			o3[1].origine = new APoint(300.0,350.0); //météorite 
@@ -145,7 +144,7 @@ public class AffichageJeu extends JPanel{
 					g.drawImage(o3[i].image,(int)(o3[i].origine.x),(int)(o3[i].origine.y),100,100,null);
 					}
 			}
-			toucheObjetCeleste(o3);
+			toucheElementMarin(o3);
 		}else if(f.numNiveau==4){
 			o4[0].origine = new APoint(450.0,400.0); //carburant
 			o4[1].origine = new APoint(350.0,200.0); //météorite
@@ -161,7 +160,7 @@ public class AffichageJeu extends JPanel{
 					g.drawImage(o4[i].image,(int)(o4[i].origine.x),(int)(o4[i].origine.y),100,100,null);
 				}
 			}
-			toucheObjetCeleste(o4);
+			toucheElementMarin(o4);
 		}else if(f.numNiveau==5){
 			o5[0].origine = new APoint(600.0,325.0); //carburant
 			o5[1].origine = new APoint(300.0,650.0); //meteorite
@@ -178,16 +177,16 @@ public class AffichageJeu extends JPanel{
 					g.drawImage(o5[i].image,(int)(o5[i].origine.x),(int)(o5[i].origine.y),100,100,null);
 				}
 			}
-			toucheObjetCeleste(o5);
+			toucheElementMarin(o5);
 		}
 	}
 	
 	/**
-   * \fn void toucheObjetCeleste(ObjetCelestes[] o) : méthode gérant le contact entre le rover et un objet céleste
+   * \fn void toucheElementMarin(ElementMarin[] o) : méthode gérant le contact entre le rover et un objet céleste
    * 
-   * @param ObjetCelestes[] o : tableau contenant les objets célestes du niveau en cours
+   * @param ElementMarin[] o : tableau contenant les objets marins du niveau en cours
    */ 
-	public void toucheObjetCeleste (ObjetsCelestes [] o){
+	public void toucheElementMarin (ElementMarin[] o){
         
         		for(int i=0;i<o.length;i++){
 				
@@ -195,23 +194,23 @@ public class AffichageJeu extends JPanel{
 				
 				if(
 				//coin en haut à gauche
-				((int)(f.Perseverance.origine.x+30)>=(int)(o[i].origine.x) && (int)(f.Perseverance.origine.x+30)<=(int)(o[i].origine.x)+100 &&
-				(int)(f.Perseverance.origine.y+30)>=(int)(o[i].origine.y) && (int)(f.Perseverance.origine.y+30)<=(int)(o[i].origine.y)+100) || 
+				((int)(f.Nemo.origine.x+30)>=(int)(o[i].origine.x) && (int)(f.Nemo.origine.x+30)<=(int)(o[i].origine.x)+100 &&
+				(int)(f.Nemo.origine.y+30)>=(int)(o[i].origine.y) && (int)(f.Nemo.origine.y+30)<=(int)(o[i].origine.y)+100) || 
 				//coin en haut à droite
-				((int)(f.Perseverance.origine.x+f.Perseverance.largeur-30)>=(int)(o[i].origine.x) && (int)(f.Perseverance.origine.x+f.Perseverance.largeur-30)<=(int)(o[i].origine.x)+100 && 
-				(int)(f.Perseverance.origine.y+30)>=(int)(o[i].origine.y) && (int)(f.Perseverance.origine.y+30)<=(int)(o[i].origine.y)+100) ||
+				((int)(f.Nemo.origine.x+f.Nemo.largeur-30)>=(int)(o[i].origine.x) && (int)(f.Nemo.origine.x+f.Nemo.largeur-30)<=(int)(o[i].origine.x)+100 && 
+				(int)(f.Nemo.origine.y+30)>=(int)(o[i].origine.y) && (int)(f.Nemo.origine.y+30)<=(int)(o[i].origine.y)+100) ||
 				//coin en bas à droite
-				((int)(f.Perseverance.origine.x+f.Perseverance.largeur-30)>=(int)(o[i].origine.x) && (int)(f.Perseverance.origine.x+f.Perseverance.largeur-30)<=(int)(o[i].origine.x)+100 && 
-				(int)(f.Perseverance.origine.y+f.Perseverance.hauteur-30)>=(int)(o[i].origine.y) && (int)(f.Perseverance.origine.y+f.Perseverance.hauteur-30)<=(int)(o[i].origine.y)+100) ||
+				((int)(f.Nemo.origine.x+f.Nemo.largeur-30)>=(int)(o[i].origine.x) && (int)(f.Nemo.origine.x+f.Nemo.largeur-30)<=(int)(o[i].origine.x)+100 && 
+				(int)(f.Nemo.origine.y+f.Nemo.hauteur-30)>=(int)(o[i].origine.y) && (int)(f.Nemo.origine.y+f.Nemo.hauteur-30)<=(int)(o[i].origine.y)+100) ||
 				//coin en bas à gauche
-				((int)(f.Perseverance.origine.x+30)>=(int)(o[i].origine.x) && (int)(f.Perseverance.origine.x+30)<=(int)(o[i].origine.x)+100 && 
-				(int)(f.Perseverance.origine.y+f.Perseverance.hauteur-30)>=(int)(o[i].origine.y) && (int)(f.Perseverance.origine.y+f.Perseverance.hauteur-30)<=(int)(o[i].origine.y)+100)){
+				((int)(f.Nemo.origine.x+30)>=(int)(o[i].origine.x) && (int)(f.Nemo.origine.x+30)<=(int)(o[i].origine.x)+100 && 
+				(int)(f.Nemo.origine.y+f.Nemo.hauteur-30)>=(int)(o[i].origine.y) && (int)(f.Nemo.origine.y+f.Nemo.hauteur-30)<=(int)(o[i].origine.y)+100)){
 					
 					//on lance la méthode action de cet objet, 1 seule fois par objet
 					if(o[i] instanceof Carburant){
-						if(touchCarbu==false){
+						if(toucherNourriture==false){
 							o[i].action();
-							touchCarbu=true;
+							toucherNourriture=true;
 						}
 					}else{
 						o[i].action();
