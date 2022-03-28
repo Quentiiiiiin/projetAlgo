7b3ba8d92ff26b0b8b1a6905d7f03d0cf57a0190
FlorentP <florentpaybou@outlook.fr>
2022-03-28 08:30:32 +0200

    poisson

--- Poisson.java
+++ Poisson.java
@@ -33,7 +33,7 @@ public class Poisson{
 	}
 	
 	/**
-   * \fn void ChangePosition(int vent, int vitesseI, int angleI, double t, double g) : méthode gérant l'actualisation de la position du poisson pour le déplacement de celui-ci
+   * \fn void ChangePosition(int courant, int vitesseI, int angleI, double t, double viscosite) : méthode gérant l'actualisation de la position du poisson pour le déplacement de celui-ci
    * 
    * @param int courant : courant du niveau en cours
    * @param int vitesseI : vitesse initiale du Poisson
@@ -41,7 +41,7 @@ public class Poisson{
    * @param double t : temps depuis le départ du Poisson
    * @param double viscosite : viscosité du milieu en cours
    */ 
-	public void ChangePosition(int courant, int vitesseI, int angleI, double t, double viscosite){//méthode effectuant le changement de la position du rover en fonction du temps et des paramètres du niveau en cours
+	public void ChangePosition(int courant, int vitesseI, int angleI, double t, double viscosite){//méthode effectuant le changement de la position du poisson en fonction du temps et des paramètres du niveau en cours
 				
         origine.x = viscosite*Courantx*Math.pow(t,2) + vitesseI * Math.cos (Math.toRadians(angleI)) + 0;
         origine.y = viscosite*Couranty*Math.pow(t,2) - vitesseI * Math.sin (Math.toRadians(angleI)) + 0;
